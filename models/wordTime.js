50a59cb0ff2b1edec4336b569dafaa0ddcedafe0
Biao <biaofu@thoughtworks.com>
2024-02-06 10:52:21 +0800

    fix

--- models/wordTime.js
+++ models/wordTime.js
@@ -3,5 +3,5 @@ module.exports = {
     T1: 't1',   // +5m
     T2: 't2',   // +30m
     T3: 't3',   // 12h
-    CrossDays: 'crossD ays' // 1d, 2d, 4d, 7d, 15d
+    CrossDays: 'crossDays' // 1d, 2d, 4d, 7d, 15d
 }
