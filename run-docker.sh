772980127941c0adff66c94202befa4a4bdea464
Biao <biaofu@thoughtworks.com>
2024-02-18 14:40:53 +0800

    update docker compose

--- run-docker.sh
+++ run-docker.sh
@@ -1,1 +1,1 @@
-docker run -d -p 3401:3401 -e PORT=3401 -e MMRB_ENV=docker -e MMRB_DB_USERNAME=mmr-user -e MMRB_DB_PASSWORD=mmr-password --network mmr-network --name memorizend memorizend:0.0.1
+docker run -d -p 3401:3401 -e MMRB_ENV=docker -e MMRB_DB_USERNAME=mmr-user -e MMRB_DB_PASSWORD=mmr-password --network mmr-network --name memorizend memorizend:0.0.1
--- src/config/env.config.js
+++ src/config/env.config.js
@@ -3,7 +3,7 @@ let mongodbUrl;
 switch (process.env.MMRB_ENV) {
     case "docker":
         // this connects mongodb through docker bridge network which is inside the mongodb container, thus can only work with the default port 27017.
-        mongodbUrl = `mongodb://${process.env.MMRB_DB_USERNAME}:${process.env.MMRB_DB_PASSWORD}@mmrb-mongodb:27017/mmrbackendb`;
+        mongodbUrl = `mongodb://${process.env.MMRB_DB_USERNAME}:${process.env.MMRB_DB_PASSWORD}@mmrbackendb:27017/mmrbackendb`;
         break;
     default:
         mongodbUrl = `mongodb://${process.env.MMRB_DB_USERNAME}:${process.env.MMRB_DB_PASSWORD}@127.0.0.1:27098/mmrbackendb`;
