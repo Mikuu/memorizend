7e425237f4b8d21964632abb0c849bc763e436f2
Biao <biaofu@thoughtworks.com>
2024-02-19 16:13:10 +0800

    update config api

--- src/routes/config.js
+++ src/routes/config.js
@@ -5,6 +5,29 @@ const { check, query, validationResult } = require("express-validator");
 const router = express.Router();
 const configService = require("../services/config");
 
+router.get("", [],
+    catchAsync(async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(StatusCodes.BAD_REQUEST).json(generalResponse(errors.array()).failed);
+        }
+
+        const config = await configService.getConfig();
+        const responseBody = {
+            config: {
+                wordsPerDay: config.wordsPerDay,
+                t0shiftTime: config.t0shiftTime,
+                t1shiftTime: config.t1shiftTime,
+                t2shiftTime: config.t2shiftTime,
+                t3shiftTime: config.t3shiftTime,
+                t4shiftTime: config.t4shiftTime,
+            }
+        }
+
+        res.status(StatusCodes.OK).send(responseBody);
+    })
+);
+
 router.post("/init", [],
     catchAsync(async (req, res, next) => {
       const errors = validationResult(req);
@@ -17,5 +40,27 @@ router.post("/init", [],
       res.status(StatusCodes.OK).send({});
     })
 );
+
+router.post("", [
+        check("config.wordsPerDay", "wordsPerDay must be provided").isNumeric(),
+        check("config.t0shiftTime", "t0shiftTime must be provided").isNumeric(),
+        check("config.t1shiftTime", "t1shiftTime must be provided").isNumeric(),
+        check("config.t2shiftTime", "t2shiftTime must be provided").isNumeric(),
+        check("config.t3shiftTime", "t3shiftTime must be provided").isNumeric(),
+        check("config.t4shiftTime", "t4shiftTime must be provided").isArray(),
+    ],
+    catchAsync(async (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(StatusCodes.BAD_REQUEST).json(generalResponse(errors.array()).failed);
+      }
+
+      const modifiedCount = await configService.updateConfig(req.body.config);
+
+      res.status(StatusCodes.OK).send({ updatedCount: modifiedCount });
+    })
+);
+
+
 
 module.exports = router;
--- src/services/config.js
+++ src/services/config.js
@@ -3,6 +3,10 @@ const { ConfigSchema } = require("../models/config");
 
 const Config = new mongoose.model('Config', ConfigSchema);
 
+const getConfig = async () => {
+    return await Config.findOne({ configName: "general" });
+};
+
 const initializeConfig = async () => {
     const config = await Config();
     return await config.save();
@@ -13,7 +17,22 @@ const getWordsPerDay = async () => {
     return config.wordsPerDay;
 };
 
+const updateConfig = async (config) => {
+    const result = await Config.updateOne({ configName: "general" }, {
+        wordsPerDay: config.wordsPerDay,
+        t0shiftTime: config.t0shiftTime,
+        t1shiftTime: config.t1shiftTime,
+        t2shiftTime: config.t2shiftTime,
+        t3shiftTime: config.t3shiftTime,
+        t4shiftTime: config.t4shiftTime,
+    });
+
+    return result.modifiedCount;
+};
+
 module.exports = {
+    getConfig,
     initializeConfig,
     getWordsPerDay,
+    updateConfig,
 }
